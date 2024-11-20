fb3dd588c1d6575c90d2542dfad9acee3168a75d
terryzhang1015 <terryzhang1015@outlook.com>
2024-11-20 14:35:54 +0800

    GPT

--- gpt_task.py
+++ gpt_task.py
@@ -9,8 +9,10 @@ import csv
 
 
 MODEL = "gpt-35-turbo"
-FILE_PATH = 'dataset/shuffled_A_B_relation_train.csv'
-with open(FILE_PATH, 'r', encoding='utf-8') as file:
+PROMPT_PATH = 'prompt'
+DATA_PATH = 'dataset/shuffled_A_B_relation_train.csv'
+
+with open(DATA_PATH, 'r', encoding='utf-8') as file:
     reader = csv.DictReader(file)
     csv_table = list(reader)
 
@@ -18,15 +20,14 @@ df = pd.DataFrame(csv_table)
 df['predict']=0
 df['EventB'] = df['EventB'].str.replace("[", "", regex=False).str.replace("]", "", regex=False).str.replace("'", "", regex=False).str.strip()
 df.head(2)
-folder_path = 'dataset'
 #df = pd.DataFrame()
 pange = pd.DataFrame()
 
 prompt_loc = {
-    "prototype": r"/content/drive/MyDrive/Colab Notebooks/comp4211 project/prompt/prototype01.txt",
-    "zero-shot": r"/content/drive/MyDrive/Colab Notebooks/comp4211 project/prompt/zero_shot.txt",
-    "one-shot": r"/content/drive/MyDrive/Colab Notebooks/comp4211 project/prompt/one_shot.txt",
-    "two-shot": r"/content/drive/MyDrive/Colab Notebooks/comp4211 project/prompt/two_shot.txt",
+    "zero-shot": f"{PROMPT_PATH}/zero_shot.txt",
+    "one-shot": f"{PROMPT_PATH}/one_shot.txt",
+    "prototype": f"{PROMPT_PATH}/prototype01.txt",
+    "two-shot": f"{PROMPT_PATH}/two_shot.txt",
 }
 
 client = AzureOpenAI(
@@ -61,11 +62,17 @@ class GPT_QUERY:
                 votes_table[vote] = 1
         return max(votes_table, key=votes_table.get)
 
-    def gpt(self, x, prompt, processor):
+    def gpt(self, x, prompt, processor, cot=False):
+        message = {
+            "role": "user",
+            "content": prompt + x + "\nAnswer:" +
+                (" Let's think step by step." if cot else ""),
+        }
+        # print(message)
         response = client.chat.completions.create(
             model="gpt-35-turbo",
             messages=[
-                {"role": "user", "content": prompt + x + "\nAnswer:"},
+                message,
             ]
         )
         #print(prompt + x + "\nAnswer:")
@@ -73,7 +80,7 @@ class GPT_QUERY:
         clean_sentence = processor(original)
         return clean_sentence
 
-    def predict(self, prompt_type, processor=categorical_clean, vote=1):    #############
+    def predict(self, prompt_type, processor=categorical_clean, vote=1, cot=False):
         try:
             set_size, table = self.set_size, self.table
             #prompt = prompt_loc[prompt_type]
@@ -84,12 +91,12 @@ class GPT_QUERY:
                     joined_sentence = " == ".join(sentences)
                     try:
                         table.at[index_A, 'predict'] = self.majority_vote([ \
-                            self.gpt(joined_sentence, prompt, processor) for _ in range(vote)])
+                            self.gpt(joined_sentence, prompt, processor, cot) for _ in range(vote)])
                     except Exception as error:
                         print(f"An error occurred at index {index_A}:", error)
                     bar.update(1)
         finally:
-            table.head(set_size)[['predict', 'relation', 'EventA', 'EventB']].to_csv(f"{prompt_type}.csv")
+            table.head(set_size)[['predict', 'relation', 'EventA', 'EventB']].to_csv(f"results/{prompt_type}_cot_{cot}.csv")
             print(table.head(set_size)[['predict', 'relation']])
 
     def estimate(self, ptf=False):
@@ -113,21 +120,11 @@ for i in category1:
     print('EventA:'+second_row['EventA']+'  EventB:'+second_row['EventB']+'  relation:'+second_row['relation'])
 full_table=df
 
-#prototype
-prompt_list = {name: open(prompt, "r").read() for name, prompt in prompt_loc.items()}
-gpt_client = GPT_QUERY(3, full_table)
-gpt_client.predict("prototype", vote=3)
-gpt_client.estimate()
-
-
 prompt_list = {name: open(prompt, "r").read() for name, prompt in prompt_loc.items()}
 # zero-shot, one-shot, two-shot
 def run_predict(type : str):
-    precision=gpt_client.estimate(True)
-    print(precision)
-    #zero-shot
-    gpt_client = GPT_QUERY(2, full_table)
+    gpt_client = GPT_QUERY(1, full_table)
     gpt_client.predict(type, vote=1)
     return gpt_client.estimate()
 
-print(run_predict("one-shot"))
+print(run_predict("zero-shot"))
